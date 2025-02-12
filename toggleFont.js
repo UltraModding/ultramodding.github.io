1d46b1d974cbc1ac8ad92bb8430308c5c8efe8a0
dolfelive <dolfelive@gmail.com>
2025-02-12 23:08:49 +1100

    Orginisation

--- toggleFont.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleFontButton = document.getElementById('toggleFontButton');
-    const body = document.body;
-
-    toggleFontButton.addEventListener('click', () => {
-        body.classList.toggle('vcr-font');
-    });
-});

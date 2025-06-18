bb9a2c77278a95e44b9b0db08b5801d745664642
dolfelive <dolfelive@gmail.com>
2025-06-18 22:26:28 +1000

    Deploy website - based on 8560a66d4eb9f3bdbed3ddc55ae1de46c8eebbef

--- README.md
+++
@@ -1,8 +0,0 @@
-## ULTRAKILL Modding Wiki!
-### Still WIP
-
-to run locally just use liveserver vs code extension
-
-todo:
-
-make the os specific options more evident that they differ between os, perhaps a box that can also be expanded and retracted?  
--- Scripts/LiveMarkdownLoader/Collector.js
+++
@@ -1,9 +0,0 @@
-import MarkdownConverter  from "/Scripts/runtimeMarkdown.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-    const converter = new MarkdownConverter();
-    
-    const test = converter.parse("# Sample\n\n**Bold Text**")
-    const html = converter.convertToHTML(test)
-    document.querySelector('.text').innerHTML = html;
-});
